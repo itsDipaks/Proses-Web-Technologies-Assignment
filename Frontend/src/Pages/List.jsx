c8d6aee0bf8b15af911b6cca3e0da1b48424a122
Dipak Pawar <itsdipakpawar4206@gmail.com>
2023-06-13 21:16:11 +0530

    Add User and List user Table Created

--- Frontend/src/Pages/List.jsx
+++ Frontend/src/Pages/List.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
 
 const List = () => {
   return (
-    <div>List</div>
-  )
-}
+    <>
+      <div class="relative overflow-x-auto w-full p-14">
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" class="px-6 py-3">
+                User name
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Mobile Number
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Email
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Edite
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Edite
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="bg-white border-b  bg-gray-800  ">
+              <th
+                scope="row"
+                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-white"
+              >
+                Dipak Pawar
+              </th>
+              <td class="px-6 py-4">8600405446</td>
+              <td class="px-6 py-4">Itsdipaksgalexyj2@gmail.com</td>
+              <td class="px-6 py-4">
+                <button>Edit</button>
+              </td>
+              <td class="px-6 py-4">
+                <button>Delete</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
 
-export default List
+export default List;
